c414a43d2c9cea8efd0ef4f953606488e0e0f933
foolspec <uk@doj.one>
2026-03-30 18:45:54 -0400

    Add files via upload

---
+++ saveweb2zip-com-kodeimmersive-com/kodeimmersive.com/Signature.js
@@ -0,0 +1,11 @@
+const micheliniStyles = 'padding: 10px; color: #fff; background-color: #ff2c2c;'
+const malvahStyles = 'padding: 10px; color: #fff; background-color: #171717;'
+
+console.clear()
+console.log('')
+console.log( 'Developed by')
+console.log('%cfrancescomichelini.com', micheliniStyles)
+console.log('')
+console.log( 'Designed by')
+console.log('%cmalvah.co', malvahStyles)
+console.log('')
